ed846b4f0c74eb925df080d9cb5343579f6988a0
Victor Rueda <vialruma1985@gmail.com>
2023-01-19 15:04:01 -0500

    Installation and configuration of the nextui library

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,51 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { DocumentContext, DocumentInitialProps, Html, Head, Main, NextScript } from 'next/document'
+import {CssBaseline} from '@nextui-org/react';
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx)
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return {
+      ...initialProps,
+      styles: <>{initialProps.styles}</>
+    }
+
+    
+  }
+
+  render() {
+    return (
+      <Html lang='en'>
+        <Head>
+          {CssBaseline.flush()}
+        </Head>  
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
+
+
+
+// import { Html, Head, Main, NextScript } from 'next/document'
+// import {CssBaseline} from '@nextui-org/react';
+
+// export default function Document() {
+//   return (
+//     <Html lang="en">
+//       <Head>
+       
+//       </Head>
+//       <body>
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   )
+// }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@nextui-org/react";
 
 
 export default function HomePage() {
   return (
     <>
       <h1>Home page</h1>
+      <Button color='gradient'>Send</Button>
     </>
   )
 }
