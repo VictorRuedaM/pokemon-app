f2650963318b80f98c26aa2dd126b11ea7306877
Victor Rueda <vialruma1985@gmail.com>
2023-01-20 16:03:01 -0500

    Component Card was created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,49 @@
+import { FC } from 'react';
 import {GetStaticProps} from 'next';
-import { MainLayout } from '../components/layouts';
+import { Card, Grid, Row, Text } from '@nextui-org/react';
 import { pokeApi } from '../api';
-import { PokemonListResponse } from '@/interfaces';
+import { PokemonListResponse, SmallPokemon } from '@/interfaces';
+import { MainLayout } from '../components/layouts';
+import Image from 'next/image';
 
 
+interface Props {
+  pokemons: SmallPokemon[];
+}
+const HomePage: FC<Props> = ({pokemons}) => {
 
-export default function HomePage(props) {
-
-  console.log({props});
+  
   return (
     <MainLayout title={'Pokemon list'}>
-     <ul>
-      <li>pokemon</li>
-      <li>pokemon</li>
-      <li>pokemon</li>
-      <li>pokemon</li>
-      <li>pokemon</li>
-      <li>pokemon</li>
-     </ul>
+     <Grid.Container gap={2} justify='flex-start'>
+      {
+        pokemons.map(({id, name, img}) => (
+          <Grid xs={6} sm={3} md={2} xl={1} key={id}>
+            <Card isHoverable isPressable>
+              <Card.Body css={{p: 1}}>
+                <Card.Image src={img} width='100%' height={140}/>
+              </Card.Body>
+              <Card.Footer>
+                <Row justify='space-between'>
+                  <Text transform='capitalize'css={{
+                     textGradient: "55deg, $blue900 -20%, $yellow600 60%",
+                    }}>{name}
+                  </Text>
+                  <Text># {id}</Text>
+                </Row>
+              </Card.Footer>
+            </Card>
+
+          </Grid>
+        ))
+      }
+      
+     </Grid.Container>
     </MainLayout>
   )
 }
 
-
+export default HomePage;
 // You should use getStaticProps when:
 //- The data required to render the page is available at build time ahead of a user’s request.
 //- The data comes from a headless CMS.
@@ -34,10 +55,17 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
 
   const {data} = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151')
   // const { data } = await  // your fetch function here 
-  
+  const pokemons = data.results.map((pokemon, index) => (
+    {
+      ...pokemon,
+      id: index + 1,
+      img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index+1}.svg`
+    }
+  ));
+
   return {
     props: {
-      pokemons: data.results
+      pokemons
     }
   }
 }
