a862f499316ed7fd2296af52813d4727b4bcac3e
Victor Rueda <vialruma1985@gmail.com>
2023-01-20 13:38:00 -0500

    added getStaticProps, api and interface pokemon-list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,43 @@
-import { Button } from "@nextui-org/react";
+import {GetStaticProps} from 'next';
 import { MainLayout } from '../components/layouts';
+import { pokeApi } from '../api';
+import { PokemonListResponse } from '@/interfaces';
 
 
-export default function HomePage() {
+
+export default function HomePage(props) {
+
+  console.log({props});
   return (
     <MainLayout title={'Pokemon list'}>
-      <h1>Home page</h1>
-      <Button color='gradient'>Send</Button>
+     <ul>
+      <li>pokemon</li>
+      <li>pokemon</li>
+      <li>pokemon</li>
+      <li>pokemon</li>
+      <li>pokemon</li>
+      <li>pokemon</li>
+     </ul>
     </MainLayout>
   )
+}
+
+
+// You should use getStaticProps when:
+//- The data required to render the page is available at build time ahead of a user’s request.
+//- The data comes from a headless CMS.
+//- The data can be publicly cached (not user-specific).
+//- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
+
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+
+  const {data} = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151')
+  // const { data } = await  // your fetch function here 
+  
+  return {
+    props: {
+      pokemons: data.results
+    }
+  }
 }
