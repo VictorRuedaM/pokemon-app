a55f7ab7e681a14c833ca9e82e933ee886be2148
Victor Rueda <vialruma1985@gmail.com>
2023-02-13 19:01:23 -0500

    added Card pokemons in favorites component

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,8 +1,8 @@
 import { MainLayout } from '@/components/layouts';
-import {NoFavorites} from '@/components/ui';
-
-import { Container, Image, Text } from '@nextui-org/react';
 import React, { useEffect, useState } from 'react'
+
+import { Card, Container, Grid, Image, Text } from '@nextui-org/react';
+import {NoFavorites} from '@/components/ui';
 import { localFavorites } from '@/utils';
 
 const Favorites = () => {
@@ -16,8 +16,37 @@ const Favorites = () => {
   return (
     
     <MainLayout title={'Favorites'}>
+
+      {
+        favoritePokemons.length === 0 
+        ? <NoFavorites/>
+        : (
+          <Grid.Container gap={2} direction='row' justify='flex-start'>
+            
+            {
+              favoritePokemons.map(id => (
+                
+                <Grid key={id} xl={1} xs={6} sm={3} md={2} >
+                  <Card isHoverable isPressable css={{padding:10, background: '$gradient'}}>
+                    <Card.Image
+                      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+                      width={'100%'}
+                      height={140}
+
+                    />
+
+                  </Card>
+
+                </Grid>
+              ))
+            }
+
+          </Grid.Container>
+        )
+
+      }
       
-      <NoFavorites/>
+      
     </MainLayout>
   )
 }
